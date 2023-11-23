372c44956c70ef8c659b15f662b33f50f72df96f
FinnleyNash <finnleynash.nash@gmail.com>
2023-11-23 18:31:38 +0000

    nav-done

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/logo1.jpg';
 
 const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
     return (
-        <nav>
-            <a href='/'>
+        <nav className={`navbar ${menuOpen ? "open" : ""}`}>
+            <a href='/' className='logo'>
                 <img src={logo} alt='logo'/>
             </a>
 
-        <div>
+
+
+        <div className="menu-icon" onClick={toggleMenu}>
+            <div className='bar'></div>
+            <div className='bar'></div>
             <div className='bar'></div>
         </div>
 
 
-
-
-
-
-
-
+        <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+            <li>
+                <a href='/'>Home</a>
+            </li>
+            <li>
+                <a href='/'>Reservations</a>
+            </li>
+            <li>
+                <a href='/'>Order Online</a>
+            </li>
+            <li>
+                <a href='/'>Menu</a>
+            </li>
+            <li>
+                <a href='/'>About Us</a>
+            </li>
+            <li>
+                <a href='/'>Reviews</a>
+            </li>
+            <li>
+                <a href='/'>Login</a>
+            </li>
+        </ul>
         </nav>
     );
 };
