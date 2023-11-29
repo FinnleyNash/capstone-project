36b8d04ffd005f2f960d059be3106def8b2c13a9
FinnleyNash <finnleynash.nash@gmail.com>
2023-11-29 15:26:35 +0000

    final-changes

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,30 @@
 import React from "react";
 import recipes from "../recipes";
+import Swal from "sweetalert2";
 
 const Menu = () => {
+
+    const handleOrder = (id) => {
+        console.log(id, "id is checked");
+        Swal.fire({
+            title: "Are you sure?",
+            text: "If you change your mind, you can edit your basket at checkout",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Add this to my order"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Added to your basket",
+                text: "Great choice!",
+                icon: "success"
+              });
+            }
+          });
+    }
+
     return (
         <div className="menu-container">
             <div className="menu-header">
@@ -20,7 +43,7 @@ const Menu = () => {
                         <p>{recipe.price}</p>
                     </div>
                     <p>{recipe.description}</p>
-                    <button className="orderbtn">Order Here</button>
+                    <button className="orderbtn" onClick={() => handleOrder(recipe.id)}>Order Here</button>
                     </div>
                 </div>)
             }
